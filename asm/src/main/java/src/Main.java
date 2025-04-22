acbdc4159fab8f9f41512f1c81d09ffd5e4c467d
JakobPuc <puc.jakob1@gmail.com>
2025-04-22 22:49:37 +0200

    changed some things

--- asm/src/main/java/src/Main.java
+++ asm/src/main/java/src/Main.java
@@ -1,6 +1,11 @@
 package src;
 
+
 public class Main {
+    public static final int sizeOfStack = 4; // depth of stack
+    
+    
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
